8ca446c0875ffc12742dd0b39b715dd320336ef0
Matheus Sales <proxgames00@gmail.com>
2023-11-18 22:54:44 +0000

    First classes of training

---
+++ src/main/java/org/training/TestProcessAndCasting.java
@@ -0,0 +1,16 @@
+package org.training;
+
+public class TestProcessAndCasting {
+    public static void main(String[] args) {
+
+        int a, b;
+        double result;
+
+        a = 5;
+        b = 2;
+
+        result = (double) a / b; // Casting example
+
+        System.out.println(result);
+    }
+}
